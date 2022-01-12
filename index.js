368a5f7c3c17970b60220f53b155fa4530ad788d
JessicaAltman <89044540+JessicaAltman@users.noreply.github.com>
2022-01-12 12:17:22 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+$("#toggle").on("click", function(){
+    $(".userResearch").slideToggle();
+})
+
+$("#toggle1").on("click", function(){
+    $(".userInterface").slideToggle();
+
+})
+
+$("#toggle2").on("click", function(){
+    $(".webDevelopment").slideToggle();
+
+})
+
+$("#toggle3").on("click", function(){
+    $(".potluck").slideToggle();
+
+})
+
+$("#toggle4").on("click", function(){
+    $(".usda").slideToggle();
+
+})
+
+$("#toggle5").on("click", function(){
+    $(".pinkSistas").slideToggle();
+
+})
